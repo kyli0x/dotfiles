6c85d9e49301f0d160af0ae64e92bfad5e471026
kyli0x <kyli0x@protonmail.ch>
2021-05-31 00:35:14 -0400

    update

--- install.sh
+++ install.sh
@@ -4,13 +4,22 @@
 # License: GNU GPLv3
 
 cp .aliases ~/.aliases
-cp bash/.bashrc ~/.bashrc
+cp bash/.* ~/
+
+mkdir ~/.config/alacritty && cp alacritty/.config/alacritty/alacritty.yml && ln -s alacritty/.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+mkdir ~/.config/neofetch
+mkdir ~/.config/nvim
+mkdir ~/.config/picom
+mkdir ~/.config/polybar
+mkdir ~/.config/ranger
+mkdir ~/.config/suckless
+mkdir ~/.config/zsh
+
+
 cp zsh/.zshrc ~/.zshrc
 cp zsh/.zprofile ~/.zprofile
-cp .oh-my-zsh/themes/kyli0x.zsh-theme ~/.oh-my-zsh/themes
 cp vim/.vimrc ~/.vimrc
-cp -r alacritty/.config/alacritty ~/.config
 cp -r neofetch/.config/neofetch/ ~/.config
-cp radare2/.radare2rc ~/.radare2rc
 cp -r rofi/.config/rofi/config.rasi ~/.config
 cp -r bpytop/.config/* ~/.config
